b62ac28d29e8314d7cba7a96192585fdbac169ab
minseon6371 <66056874+minseon6371@users.noreply.github.com>
2021-08-11 05:51:06 +0900

    Update MinseonSite.js

--- MinseonSite.js
+++ MinseonSite.js
@@ -1,3 +1,25 @@
+window.addEventListener("wheel", function(e){
+    e.preventDefault();
+},{passive : false});
+
+var $html = $("html");
+var page = 1;
+var lastPage = $(".section").length;
+$html.animate({scrollTop:0},10);
+
+$(window).on("wheel", function(e){
+    if($html.is(":animated")) return;
+    if(e.originalEvent.deltaY > 0){
+        if(page== lastPage) return;
+        page++;
+    }else if(e.originalEvent.deltaY < 0){
+        if(page == 1) return;
+        page--;
+    }
+    var posTop = (page-1) * $(window).height();
+    $html.animate({scrollTop : posTop});
+});
+
 $(function(){
     $('#esfjText').click(function(){
         $('#esfjText').hide();
@@ -53,7 +75,7 @@ $(function(){
 });
 
 $(function(){
-    $('#songPop').mouseleave(function(){
+    $('#songPop').click(function(){
         $('#songPop').hide();
         $('#songText').show();
     });
